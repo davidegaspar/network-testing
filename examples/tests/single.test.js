3048367e6bee21fd936fba021628ae9f89f9a4a2
davidegaspar <git@davidegaspar.com>
2021-08-09 20:05:40 +0100

    wip

--- examples/tests/single.test.js
+++ examples/tests/single.test.js
@@ -1,38 +1,65 @@
 const expect = require("chai").expect;
 const { NetworkTest, NetworkEvents } = require("../../index");
 
-const requestTimeout = 1000;
-
 describe("Single Example", () => {
   beforeEach(() => {
-    network = new NetworkTest({}, requestTimeout);
+    network = new NetworkTest({}, 1000, 2);
   });
 
-  it("should connect", (done) => {
+  it("should connect: pass", (done) => {
     network.whenConnectTo("davidegaspar.dev", 443);
 
     network.then((event) => {
       expect(event).to.equal(NetworkEvents.CONNECT);
-      done();
-    });
+    }, done);
   });
+  it("should connect: fail due to error", (done) => {
+    network.whenConnectTo("some.host.that.does.not.exist", 443);
 
-  it("should timeout", (done) => {
+    network.then((event) => {
+      expect(event).to.equal(NetworkEvents.CONNECT);
+    }, done);
+  });
+  xit("should connect: fail due to timeout", (done) => {
     network.whenConnectTo("some.host.that.times.out", 443);
 
     network.then((event) => {
-      expect(event).to.equal(NetworkEvents.TIMEOUT);
-      done();
-    });
+      expect(event).to.equal(NetworkEvents.CONNECT);
+    }, done);
   });
 
-  it("should error", (done) => {
+  it("should error: pass", (done) => {
     network.whenConnectTo("some.host.that.does.not.exist", 443);
 
     network.then((event, error) => {
       expect(event).to.equal(NetworkEvents.ERROR);
       expect(error.code).to.equal("ENOTFOUND");
-      done();
-    });
+    }, done);
   });
+  it("should error: fail due to connect", (done) => {
+    //TODO: weird error
+    network.whenConnectTo("davidegaspar.dev", 443);
+
+    network.then((event, error) => {
+      expect(event).to.equal(NetworkEvents.ERROR);
+      expect(error.code).to.equal("ENOTFOUND");
+    }, done);
+  });
+  xit("should error: fail due to timeout", (done) => {
+    network.whenConnectTo("some.host.that.times.out", 443);
+
+    network.then((event, error) => {
+      expect(event).to.equal(NetworkEvents.ERROR);
+      expect(error.code).to.equal("ENOTFOUND");
+    }, done);
+  });
+
+  // xit("should timeout", (done) => {
+  //   network.whenConnectTo("some.host.that.times.out", 443);
+
+  //   network.then((event) => {
+  //     expect(event).to.equal(NetworkEvents.TIMEOUT);
+  //     done();
+  //   });
+  // });
 });
--- index.js
+++ index.js
@@ -7,28 +7,59 @@ const NetworkEvents = Object.freeze({
 });
 
 class NetworkTest extends net.Socket {
-  constructor(options, timeout) {
+  constructor(options, timeout = 1000, retries = 2) {
     super(options);
-    super.setTimeout(timeout || 1000);
+    super.setTimeout(timeout);
+    this.retries = retries;
+  }
+
+  then(ex, done) {
+    this.exFn = ex;
+    this.done = done;
+    this.on("connect", () => {
+      // console.log("connect!");
+      this.retry(NetworkEvents.CONNECT, null);
+    });
+    this.on("timeout", () => {
+      // console.log("timeout!");
+      this.retry(NetworkEvents.TIMEOUT, null);
+    });
+    this.on("error", (error) => {
+      // console.log("error!");
+      this.retry(NetworkEvents.ERROR, error);
+    });
+    this.verify();
   }
 
   whenConnectTo(host, port) {
-    super.connect(port, host);
+    this.host = host;
+    this.port = port;
   }
 
-  then(callback) {
-    this.on("connect", () => {
+  verify() {
+    super.connect(this.port, this.host);
+  }
+
+  retry(event, error) {
+    // console.log("retry", event, error);
+    try {
+      this.exFn(event, error);
+      // console.log("retry", "ok");
       this.destroy();
-      callback(NetworkEvents.CONNECT, null);
-    });
-    this.on("timeout", () => {
-      this.destroy();
-      callback(NetworkEvents.TIMEOUT, null);
-    });
-    this.on("error", (error) => {
-      this.destroy();
-      callback(NetworkEvents.ERROR, error);
-    });
+      this.done();
+    } catch (err) {
+      // console.log("retry catch", err);
+      // console.log("retries", this.retries);
+      if (this.retries > 0) {
+        // console.log("retry", "again");
+        this.retries = this.retries - 1;
+        this.verify();
+      } else {
+        // console.log("retry", "stop");
+        this.destroy();
+        throw error;
+      }
+    }
   }
 }
 
